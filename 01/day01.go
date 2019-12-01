7c14f5a71f36233e704704d5198e806d4381fe19
Thijs van der Burgt <thijs.vanderburgt@gmail.com>
2019-12-01 15:01:33 +0100

    day1

--- 01/day01.go
+++ 01/day01.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"github.com/subchord/aoc-2019/util"
+	"strconv"
+)
+
 func main() {
 
+	lines := util.ReadLines("inp")
+	numbers := make([]int, len(lines))
+	for i, line := range lines {
+		atoi, _ := strconv.Atoi(line)
+		numbers[i] = atoi
+	}
+	fmt.Println(part1(numbers))
+	fmt.Println(part2(numbers))
+}
+
+func part1(numbers []int) int {
+	sum := 0
+	for _, n := range numbers {
+		sum += n/3 - 2
+	}
+	return sum
+}
+
+func part2(numbers []int) int {
+	sum := 0
+	for _, n := range numbers {
+		f := n
+		for f > 6 {
+			f = f/3-2
+			sum += f
+		}
+	}
+	return sum
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/subchord/aoc-2019
+
+go 1.13
