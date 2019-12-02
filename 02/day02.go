09555a624dafa1fff99820311df243b39b70d120
Thijs van der Burgt <thijs.van.der.burgt@be-mobile.com>
2019-12-02 09:24:30 +0100

    day2

---
+++ 02/day02.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/subchord/aoc-2019/util"
+	"strings"
+)
+
+func main() {
+	lines := util.ReadLines("inp")
+	parts := strings.Split(lines[0], ",")
+	numbers, err := util.StringSlice(parts).ToIntSlice()
+	if err != nil {
+		return
+	}
+
+	fmt.Println(part1(append([]int(nil), numbers...)))
+	fmt.Println(part2(append([]int(nil), numbers...)))
+}
+
+func do(ints []int) error {
+	pt := 0
+	for ints[pt] != 99 {
+		if ints[pt] == 1 {
+			ints[ints[pt+3]] = ints[ints[pt+1]] + ints[ints[pt+2]]
+		} else if ints[pt] == 2 {
+			ints[ints[pt+3]] = ints[ints[pt+1]] * ints[ints[pt+2]]
+		} else {
+			return errors.New("unkown opt")
+		}
+		pt += 4
+	}
+	return nil
+}
+
+func part1(ints []int) int {
+	ints[1] = 12
+	ints[2] = 2
+	err := do(ints)
+	if err != nil {
+		return -1
+	}
+	return ints[0]
+}
+
+func part2(ints []int) int {
+	a, b := 0, 0
+out:
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			a, b = i, j
+			nums := append([]int(nil), ints...)
+			nums[1] = a
+			nums[2] = b
+			_ = do(nums)
+			if nums[0] == 19690720 {
+				break out
+			}
+		}
+	}
+
+	return 100*a + b
+}
