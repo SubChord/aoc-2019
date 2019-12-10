8de267c2cb4bcae283201ef29c2a644ba9525674
SubChord <thijs.vanderburgt@gmail.com>
2019-12-10 23:54:59 +0100

    day10

--- 10/day10.go
+++ 10/day10.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/subchord/aoc-2019/util"
 	"math"
+	"sort"
 )
 
 type Location struct {
@@ -11,16 +12,74 @@ type Location struct {
 	Y int
 }
 
+func (l Location) manhattan(l2 Location) int {
+	return int(math.Abs(float64(l.X-l2.X)) + math.Abs(float64(l.Y-l2.Y)))
+}
+
 func main() {
 	lines := util.ReadLines("inp")
 	comets := parseLines(lines)
 	location, max := part1(comets)
 	fmt.Println(max)
-	part2(location)
+	part2(location, comets)
 }
 
-func part2(location Location) {
+func part2(location Location, comets []Location) {
+	distances := make(map[float64][]Location)
 
+	// Group comets by atan2 (same line)
+	for i, comet := range comets {
+		if comet == location {
+			continue
+		}
+
+		atan2 := math.Atan2(float64(location.Y-comet.Y), float64(location.X-comet.X))
+
+		// correct atan2 so that 90deg == 0
+		atan2 += 1.5 * math.Pi
+		atan2 = math.Mod(atan2, 2*math.Pi)
+
+		_, ok := distances[atan2]
+		if !ok {
+			distances[atan2] = []Location{}
+		}
+		distances[atan2] = append(distances[atan2], comets[i])
+	}
+
+	// Sort slices based on manhattan distance
+	for _, comets := range distances {
+		sort.Slice(comets, func(i, j int) bool {
+			return comets[i].manhattan(location) < comets[j].manhattan(location)
+		})
+	}
+
+	atans := []float64{}
+	for f := range distances {
+		atans = append(atans, f)
+	}
+	sort.Float64s(atans)
+
+	count := 0
+	i := 0
+	var l Location
+	for count < 200 {
+		for {
+			key := atans[i % len(atans)]
+			locations := distances[key]
+			if len(locations) == 0 {
+				i++
+				continue
+			}
+			distances[key] = locations[1:]
+			l = locations[0]
+			i++
+			break
+		}
+
+		count++
+	}
+
+	fmt.Println(l.X*100 + l.Y)
 }
 
 func part1(comets []Location) (Location, int) {
